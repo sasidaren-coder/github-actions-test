6a10061099b3def099bd51eb922ab785160b084e
Sasidaren Dinakaran <sdinakaran@psyncopate.com>
2025-05-09 16:54:50 -0400

    support multiple clouds and clusers

--- confluentcloud-resource-provisioner-tf/main.tf
+++ confluentcloud-resource-provisioner-tf/main.tf
@@ -8,13 +8,33 @@ terraform {
 }
 
 provider "confluent" {
-  alias               = "cc"
-  cloud_api_key       = var.confluent_cloud_api_key
-  cloud_api_secret    = var.confluent_cloud_api_secret
-  kafka_id            = var.kafka_cluster_id
-  kafka_rest_endpoint = var.kafka_rest_endpoint
-  kafka_api_key       = var.kafka_api_key
-  kafka_api_secret    = var.kafka_api_secret
+  alias               = "azure"
+  cloud_api_key       = var.azure_confluent_cloud_api_key
+  cloud_api_secret    = var.azure_confluent_cloud_api_secret
+  kafka_id            = var.azure_kafka_cluster_id
+  kafka_rest_endpoint = var.azure_kafka_rest_endpoint
+  kafka_api_key       = var.azure_kafka_api_key
+  kafka_api_secret    = var.azure_kafka_api_secret
+}
+
+provider "confluent" {
+  alias               = "gcp"
+  cloud_api_key       = var.gcp_confluent_cloud_api_key
+  cloud_api_secret    = var.gcp_confluent_cloud_api_secret
+  kafka_id            = var.gcp_kafka_cluster_id
+  kafka_rest_endpoint = var.gcp_kafka_rest_endpoint
+  kafka_api_key       = var.gcp_kafka_api_key
+  kafka_api_secret    = var.gcp_kafka_api_secret
+}
+
+provider "confluent" {
+  alias               = "aws"
+  cloud_api_key       = var.aws_confluent_cloud_api_key
+  cloud_api_secret    = var.aws_confluent_cloud_api_secret
+  kafka_id            = var.aws_kafka_cluster_id
+  kafka_rest_endpoint = var.aws_kafka_rest_endpoint
+  kafka_api_key       = var.aws_kafka_api_key
+  kafka_api_secret    = var.aws_kafka_api_secret
 }
 
 # locals {
@@ -23,13 +43,21 @@ provider "confluent" {
 
 locals {
   resource_config = yamldecode(file(var.resource_yaml_path))
+  cloud_provider = var.cloud_provider
+  active = {
+    azure = cloud_provider == "azure"
+    gcp   = cloud_provider == "gcp"
+    aws   = cloud_provider == "aws"
+  }
 }
 
 module "topics" {
+for_each = { for k, v in local.active : k => v if v }
+
   source = "./modules/kafka-topic"
 
   providers = {
-    confluent = confluent.cc
+    confluent = confluent[each.key]
   }
 
   topics = local.resource_config.topics
@@ -37,13 +65,25 @@ module "topics" {
 
 
 module "service_accounts" {
+  for_each = { for k, v in local.active : k => v if v }
+
   source = "./modules/kafka-serviceaccounts"
 
   providers = {
-    confluent = confluent.cc
+    confluent = confluent[each.key]
   }
 
   serviceaccounts = local.resource_config.serviceaccounts
-  kafka_cluster_id = var.kafka_cluster_id
-  cc_environment_id = var.cc_environment_id
+  
+  kafka_cluster_id   = lookup({
+    azure = var.azure_kafka_cluster_id,
+    gcp   = var.gcp_kafka_cluster_id,
+    aws   = var.aws_kafka_cluster_id
+  }, each.key)
+
+  cc_environment_id  = lookup({
+    azure = var.azure_cc_environment_id,
+    gcp   = var.gcp_cc_environment_id,
+    aws   = var.aws_cc_environment_id
+  }, each.key)
 }
--- confluentcloud-resource-provisioner-tf/variables.tf
+++ confluentcloud-resource-provisioner-tf/variables.tf
@@ -1,53 +1,110 @@
-variable "confluent_cloud_api_key" {
+# Azure
+variable "azure_confluent_cloud_api_key" {
   type        = string
   sensitive   = true
 }
 
-variable "confluent_cloud_api_secret" {
+variable "azure_confluent_cloud_api_secret" {
   type        = string
   sensitive   = true
 }
 
-variable "kafka_cluster_id" {
+variable "azure_kafka_cluster_id" {
   type = string
 }
 
-variable "kafka_rest_endpoint" {
+variable "azure_kafka_rest_endpoint" {
   type = string
 }
 
-variable "kafka_api_key" {
+variable "azure_kafka_api_key" {
   type      = string
   sensitive = true
 }
 
-variable "kafka_api_secret" {
+variable "azure_kafka_api_secret" {
   type      = string
   sensitive = true
 }
 
-variable "cc_environment_id" {
+variable "azure_cc_environment_id" {
   type = string
 }
 
+## GCP
+variable "gcp_confluent_cloud_api_key" {
+  type        = string
+  sensitive   = true
+}
+
+variable "gcp_confluent_cloud_api_secret" {
+  type        = string
+  sensitive   = true
+}
+
+variable "gcp_kafka_cluster_id" {
+  type = string
+}
+
+variable "gcp_kafka_rest_endpoint" {
+  type = string
+}
+
+variable "gcp_kafka_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "gcp_kafka_api_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "gcp_cc_environment_id" {
+  type = string
+}
+
+## AWS
+variable "aws_confluent_cloud_api_key" {
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_confluent_cloud_api_secret" {
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_kafka_cluster_id" {
+  type = string
+}
+
+variable "aws_kafka_rest_endpoint" {
+  type = string
+}
+
+variable "aws_kafka_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "aws_kafka_api_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "aws_cc_environment_id" {
+  type = string
+}
+
+variable "cloud_provider" {
+  type = string
+}
+
+# Resource Configs
 variable "resource_yaml_path" {
   type        = string
   description = "Path to the YAML config file"
 }
 
-# variable "topics" {
-#   type = list(object({
-#     topic_name         = string
-#     partitions_count   = number
-#     config             = map(string)
-#   }))
-# }
-
-# variable "serviceaccounts" {
-#   type = list(object({
-#     name      = string
-#     description      = string
-#     operation = list(string)
-#   }))
-# }
 

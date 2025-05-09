f42b3df2d45fbea4319a9853e4b2ab76eebab44b
Sasidaren Dinakaran <sdinakaran@psyncopate.com>
2025-05-09 17:51:23 -0400

    workflow changes

--- confluentcloud-resource-provisioner-tf/main.tf
+++ confluentcloud-resource-provisioner-tf/main.tf
@@ -37,51 +37,50 @@ provider "confluent" {
   kafka_api_secret    = var.aws_kafka_api_secret
 }
 
-# locals {
-#   resource_config = yamldecode(file("${path.module}/resources.yaml"))
-# }
-
 locals {
   resource_config = yamldecode(file(var.resource_yaml_path))
-  cloud_provider = var.cloud_provider
-  active = {
-    azure = cloud_provider == "azure"
-    gcp   = cloud_provider == "gcp"
-    aws   = cloud_provider == "aws"
+  cloud_provider  = var.cloud_provider
+  active_aliases = {
+    azure = var.cloud_provider == "azure"
+    gcp   = var.cloud_provider == "gcp"
+    aws   = var.cloud_provider == "aws"
   }
 }
 
 module "topics" {
-for_each = { for k, v in local.active : k => v if v }
+  for_each = { for k, v in local.active_aliases : k => v if v }
 
   source = "./modules/kafka-topic"
 
   providers = {
     confluent = confluent.azure
-  }
+    confluent = confluent.gcp
+    confluent = confluent.aws
+  }[each.key]
 
   topics = local.resource_config.topics
 }
 
-
 module "service_accounts" {
-  for_each = { for k, v in local.active : k => v if v }
+  for_each = { for k, v in local.active_aliases : k => v if v }
 
   source = "./modules/kafka-serviceaccounts"
 
   providers = {
     confluent = confluent.azure
-  }
+    confluent = confluent.gcp
+    confluent = confluent.aws
+  }[each.key]
 
   serviceaccounts = local.resource_config.serviceaccounts
 
-  kafka_cluster_id   = lookup({
+  kafka_cluster_id = lookup({
     azure = var.azure_kafka_cluster_id,
     gcp   = var.gcp_kafka_cluster_id,
     aws   = var.aws_kafka_cluster_id
   }, each.key)
 
-  cc_environment_id  = lookup({
+  cc_environment_id = lookup({
     azure = var.azure_cc_environment_id,
     gcp   = var.gcp_cc_environment_id,
     aws   = var.aws_cc_environment_id
